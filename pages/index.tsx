015601282b3f41ec18d048b94376e03840724931
aZaheer008 <kmitinstitute008@gmail.com>
2022-07-30 18:49:17 +0500

    provider setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,23 @@ import type { NextPage } from 'next'
 import { BaseLayout, NftList } from '@ui';
 import nfts from "../content/meta.json";
 import { NftMeta } from '@_types/nft';
+import { useWeb3 } from '@providers/web3';
 
 const Home: NextPage = () => {
-  return (
+  const { ethereum, provider, isLoading, contract } = useWeb3();
+
+  console.log("-----provider-----",provider);
+
+  const getAccounts = async () => {
+    const accounts = await provider!.listAccounts();
+    console.log("----------accounts : ",accounts);
+  }
+
+  if (provider){
+    getAccounts();
+  }
+  
+   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
