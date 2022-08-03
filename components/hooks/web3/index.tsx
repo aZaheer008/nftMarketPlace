3e1e6d05916046298cca5d40b1fad78d88040643
aZaheer008 <kmitinstitute008@gmail.com>
2022-08-03 08:55:36 +0500

    fetch and display owned nfts

--- components/hooks/web3/index.tsx
+++ components/hooks/web3/index.tsx
@@ -21,7 +21,16 @@ export const useNetwork = () => {
 
 export const useListedNfts = () => {
     const hooks = useHooks();
-    const swrRes = hooks.useListedNfts("");
+    const swrRes = hooks.useListedNfts();
+
+    return {
+        nfts: swrRes
+    }
+}
+
+export const useOwnedNfts = () => {
+    const hooks = useHooks();
+    const swrRes = hooks.useOwnedNfts();
 
     return {
         nfts: swrRes
